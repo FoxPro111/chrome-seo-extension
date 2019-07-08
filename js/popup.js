1bc2abae538a5e42cb04251d58c1c842bb1e3bd0
Yurii_Oliiarnyk <oliiarnyk14@gmail.com>
2019-07-08 08:40:39 +0300

    rebuild

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,25 @@
-// constants
+/*********************/
+/***** CONSTANTS *****/
+/*********************/
 const STORAGE = chrome.storage.sync;
 const TITLE = document.getElementById("title");
 const DESCRIPTION = document.getElementById("description");
 const URL = document.getElementById("url");
 const SEND_BUTTON = document.querySelector(".js-send");
 const ADD_PROFILE = document.querySelector(".js-add-profile");
+const CLEAR_BUTTON = document.querySelector(".js-clear");
+const IMAGES_MIN_WIDTH = 320;
 
-// get page information
+const INPUT_NAME = document.getElementById("input-profile");
+const INPUT_KEY = document.getElementById("input-key");
+const INPUT_SECRET = document.getElementById("input-secret");
+
+const TAB_LINKS = document.getElementsByClassName("popup__tab__link");
+const TAB_WRAPS = document.getElementsByClassName("popup__wrap");
+
+/********************************/
+/***** GET PAGE INFORMATION *****/
+/********************************/
 function getPageInformation() {
   var code =
     "var meta = document.querySelector(\"meta[name='description']\");" +
@@ -37,63 +50,75 @@ function getPageInformation() {
         // the permission to execute a content script for the current tab
         return;
       }
-      var result = results[0];
 
-      if (result.title) {
-        TITLE.innerHTML = result.title;
-      }
-
-      if (result.description) {
-        DESCRIPTION.innerHTML = result.description;
-      }
-
-      if (result.location) {
-        URL.innerHTML = result.location;
-      }
-
-      if (result.images) {
-        var imagesDOM = document.querySelector(".popup__images");
-        var imagesOutput = '<div class="popup__images-list">';
-
-        var images = result.images.slice("");
-
-        var filteredImages = images
-          .filter(function(img) {
-            return img.width > 320;
-          })
-          .sort(function(imgA, imgB) {
-            return imgA.width - imgB.width;
-          });
-
-        filteredImages.forEach(function(image, index) {
-          var imgClass = index === 0 ? " is-active" : "";
-
-          imagesOutput += '<div class="popup__images-item">';
-          imagesOutput += '<div class="js-image popup__image' + imgClass + '">';
-          imagesOutput += '<img src="' + image.src + '" />';
-          imagesOutput += image.alt.trim()
-            ? '<span class="popup__image-alt">' + image.alt + "</span>"
-            : "";
-          imagesOutput += "</div>";
-          imagesOutput += "</div>";
-        });
-
-        imagesOutput += "</div>";
-
-        imagesDOM.innerHTML = imagesOutput;
-
-        var jsImages = document.getElementsByClassName("js-image");
-        Array.prototype.forEach.call(jsImages, function(image) {
-          image.addEventListener("click", changeActiveImage);
-        });
-      }
+      renderPageInformation(results[0]);
     }
   );
 }
 
-getPageInformation();
+/***********************************/
+/***** RENDER PAGE INFORMATION *****/
+/***********************************/
+function renderPageInformation(result) {
+  if (result.title) {
+    TITLE.innerHTML = result.title;
+  }
 
-function changeActiveImage() {
+  if (result.description) {
+    DESCRIPTION.innerHTML = result.description;
+  }
+
+  if (result.location) {
+    URL.innerHTML = result.location;
+  }
+
+  if (result.images) {
+    renderImages(result.images);
+  }
+}
+
+/******************************/
+/***** RENDER PAGE IMAGES *****/
+/******************************/
+function renderImages(images) {
+  var imagesDOM = document.querySelector(".popup__images");
+  var imagesOutput = '<div class="popup__images-list">';
+
+  var filteredImages = images
+    .filter(function(img) {
+      return img.width > IMAGES_MIN_WIDTH;
+    })
+    .sort(function(imgA, imgB) {
+      return imgA.width - imgB.width;
+    });
+
+  filteredImages.forEach(function(image, index) {
+    var imgClass = index === 0 ? " is-active" : "";
+
+    imagesOutput += '<div class="popup__images-item">';
+    imagesOutput += '<div class="js-image popup__image' + imgClass + '">';
+    imagesOutput += '<img src="' + image.src + '" />';
+    imagesOutput += image.alt.trim()
+      ? '<span class="popup__image-alt">' + image.alt + "</span>"
+      : "";
+    imagesOutput += "</div>";
+    imagesOutput += "</div>";
+  });
+
+  imagesOutput += "</div>";
+
+  imagesDOM.innerHTML = imagesOutput;
+
+  var jsImages = document.getElementsByClassName("js-image");
+  Array.prototype.forEach.call(jsImages, function(image) {
+    image.addEventListener("click", changeActiveImageHandler);
+  });
+}
+
+/********************************/
+/***** CHANGE ACTIVE IMAGES *****/
+/********************************/
+function changeActiveImageHandler() {
   var jsImages = document.getElementsByClassName("js-image");
 
   Array.prototype.forEach.call(jsImages, function(image) {
@@ -103,40 +128,10 @@ function changeActiveImage() {
   this.classList.add("is-active");
 }
 
-// send button click
-SEND_BUTTON.addEventListener("click", function(event) {
-  event.preventDefault();
-
-  var title = TITLE.textContent;
-  var description = DESCRIPTION.textContent;
-  var url = URL.textContent;
-  var APP_KEY = "";
-  var APP_SECRET = "";
-  var image = "";
-
-  var select = document.getElementById("profile-select");
-  var imageEl = document.querySelector(".popup__image.is-active img");
-
-  if (imageEl) {
-    image = imageEl.src;
-  }
-
-  STORAGE.get(["profiles"], function(result) {
-    var profiles = [...result.profiles];
-
-    var profile = profiles.find(function(element) {
-      return element.id === +select.value;
-    });
-
-    APP_KEY = profile.key;
-    APP_SECRET = profile.secret;
-
-    sendNotification(APP_KEY, APP_SECRET, title, description, image, url);
-  });
-});
-
-// send request
-function sendNotification(key, secret, title, content, img, link) {
+/*******************************/
+/***** SEND NOTIFY HANDLER *****/
+/*******************************/
+function sendNotificationHandler(key, secret, title, content, img, link) {
   var data = JSON.stringify({
     payload: {
       message: content,
@@ -149,7 +144,7 @@ function sendNotification(key, secret, title, content, img, link) {
   var promise = fetch("https://uapi.gravitec.net/api/v3/push", {
     method: "POST",
     credentials: "include",
-    mode: "no-cors",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Basic " + key + ":" + secret,
@@ -168,78 +163,10 @@ function sendNotification(key, secret, title, content, img, link) {
     });
 }
 
-const tabs = document.getElementsByClassName("popup__tab__link");
-const tabs_wrap = document.getElementsByClassName("popup__wrap");
-
-Array.prototype.forEach.call(tabs, function(tab) {
-  tab.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    var target = this.getAttribute("data-tab");
-
-    Array.prototype.forEach.call(tabs, function(image) {
-      image.parentNode.classList.remove("is-active");
-    });
-
-    Array.prototype.forEach.call(tabs_wrap, function(image) {
-      image.classList.remove("is-active");
-    });
-
-    document.getElementById(target).classList.add("is-active");
-    this.parentNode.classList.add("is-active");
-  });
-});
-
-ADD_PROFILE.addEventListener("click", function(e) {
-  e.preventDefault();
-
-  var inputName = document.getElementById("input-profile");
-  var inputKEY = document.getElementById("input-key");
-  var inputSECRET = document.getElementById("input-secret");
-
-  var name = inputName.innerHTML;
-  var key = inputKEY.innerHTML;
-  var secret = inputSECRET.innerHTML;
-
-  if (name.trim() === "") {
-    inputName.classList.add("not-valid");
-
-    return;
-  }
-
-  if (key.trim() === "") {
-    inputKEY.classList.add("not-valid");
-
-    return;
-  }
-
-  if (secret.trim() === "") {
-    inputSECRET.classList.add("not-valid");
-
-    return;
-  }
-
-  inputName.innerHTML = inputKEY.innerHTML = inputSECRET.innerHTML = "";
-
-  STORAGE.get(["profiles"], function(result) {
-    var profiles = [];
-    if (typeof result.profiles !== "undefined") {
-      profiles = [...result.profiles];
-    }
-
-    profiles.push({
-      name,
-      key,
-      secret,
-      id: Math.floor(Math.random() * 100000)
-    });
-
-    STORAGE.set({ profiles: profiles });
-    initSelect();
-  });
-});
-
-function initSelect() {
+/*************************/
+/***** INIT PROFILES *****/
+/*************************/
+function initProfiles() {
   STORAGE.get(["profiles"], function(result) {
     var profiles = [];
     var selectOutput = "";
@@ -312,34 +239,41 @@ function initSelect() {
 
     var editHeader = document.getElementsByClassName("js-profile-edit");
     Array.prototype.forEach.call(editHeader, function(btn) {
-      btn.addEventListener("click", tabToggle);
+      btn.addEventListener("click", accordionToggle);
     });
 
     var deleteButtons = document.getElementsByClassName("js-profile-delete");
     Array.prototype.forEach.call(deleteButtons, function(btn) {
-      btn.addEventListener("click", profileDelete);
+      btn.addEventListener("click", profileDeleteHandler);
     });
 
     var updateButtons = document.getElementsByClassName("js-profile-update");
     Array.prototype.forEach.call(updateButtons, function(btn) {
-      btn.addEventListener("click", profileUpdate);
+      btn.addEventListener("click", profileUpdateHandler);
     });
   });
 }
 
-initSelect();
-
-const clear = document.querySelector(".js-clear");
-
-clear.addEventListener("click", function(event) {
+/*****************************/
+/***** ACCORDION HANDLER *****/
+/*****************************/
+function accordionToggle(event) {
   event.preventDefault();
 
-  STORAGE.clear();
+  var content = this.parentNode.parentNode.querySelector(".profiles__content");
 
-  initSelect();
-});
+  var allContent = document.getElementsByClassName("profiles__content");
+  Array.prototype.forEach.call(allContent, function(content) {
+    content.style.display = "none";
+  });
 
-function profileDelete(event) {
+  content.style.display = "block";
+}
+
+/**********************************/
+/***** PROFILE DELETE HANDLER *****/
+/**********************************/
+function profileDeleteHandler(event) {
   event.preventDefault();
 
   var id = +this.getAttribute("data-id");
@@ -353,24 +287,14 @@ function profileDelete(event) {
 
     STORAGE.set({ profiles: profiles });
 
-    initSelect();
+    initProfiles();
   });
 }
 
-function tabToggle(event) {
-  event.preventDefault();
-
-  var content = this.parentNode.parentNode.querySelector(".profiles__content");
-
-  var allContent = document.getElementsByClassName("profiles__content");
-  Array.prototype.forEach.call(allContent, function(content) {
-    content.style.display = "none";
-  });
-
-  content.style.display = "block";
-}
-
-function profileUpdate(event) {
+/**********************************/
+/***** PROFILE UPDATE HANDLER *****/
+/**********************************/
+function profileUpdateHandler(event) {
   event.preventDefault();
 
   var wrap = this.parentNode;
@@ -399,6 +323,140 @@ function profileUpdate(event) {
 
     STORAGE.set({ profiles: profiles });
 
-    initSelect();
+    initProfiles();
   });
 }
+
+/*******************************/
+/***** TAB CHANGE LISTENER *****/
+/*******************************/
+Array.prototype.forEach.call(TAB_LINKS, function(tab) {
+  tab.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var target = this.getAttribute("data-tab");
+
+    Array.prototype.forEach.call(TAB_LINKS, function(image) {
+      image.parentNode.classList.remove("is-active");
+    });
+
+    Array.prototype.forEach.call(TAB_WRAPS, function(image) {
+      image.classList.remove("is-active");
+    });
+
+    document.getElementById(target).classList.add("is-active");
+    this.parentNode.classList.add("is-active");
+  });
+});
+
+/*******************************/
+/***** SEND BUTTON HANDLER *****/
+/*******************************/
+SEND_BUTTON.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var title = TITLE.textContent;
+  var description = DESCRIPTION.textContent;
+  var url = URL.textContent;
+  var APP_KEY = "";
+  var APP_SECRET = "";
+  var image = "";
+
+  var select = document.getElementById("profile-select");
+  var imageEl = document.querySelector(".popup__image.is-active img");
+
+  if (imageEl) {
+    image = imageEl.src;
+  }
+
+  STORAGE.get(["profiles"], function(result) {
+    var profiles = [...result.profiles];
+
+    var profile = profiles.find(function(element) {
+      return element.id === +select.value;
+    });
+
+    APP_KEY = profile.key;
+    APP_SECRET = profile.secret;
+
+    sendNotificationHandler(
+      APP_KEY,
+      APP_SECRET,
+      title,
+      description,
+      image,
+      url
+    );
+  });
+});
+
+/***********************************/
+/***** ADD NEW PROFILE HANDLER *****/
+/***********************************/
+ADD_PROFILE.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  const name = INPUT_NAME.innerHTML;
+  const key = INPUT_KEY.innerHTML;
+  const secret = INPUT_SECRET.innerHTML;
+
+  INPUT_NAME.classList.remove("not-valid");
+  INPUT_KEY.classList.remove("not-valid");
+  INPUT_SECRET.classList.remove("not-valid");
+
+  if (name.trim() === "") {
+    INPUT_NAME.classList.add("not-valid");
+
+    return;
+  }
+
+  if (key.trim() === "") {
+    INPUT_KEY.classList.add("not-valid");
+
+    return;
+  }
+
+  if (secret.trim() === "") {
+    INPUT_SECRET.classList.add("not-valid");
+
+    return;
+  }
+
+  INPUT_NAME.innerHTML = INPUT_KEY.innerHTML = INPUT_SECRET.innerHTML = "";
+
+  STORAGE.get(["profiles"], function(result) {
+    var profiles = [];
+    if (typeof result.profiles !== "undefined") {
+      profiles = [...result.profiles];
+    }
+
+    profiles.push({
+      name,
+      key,
+      secret,
+      id: Math.floor(Math.random() * 100000)
+    });
+
+    STORAGE.set({ profiles: profiles });
+    initProfiles();
+  });
+});
+
+/******************************/
+/***** CLEAR ALL SETTiNGS *****/
+/******************************/
+CLEAR_BUTTON.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  STORAGE.clear();
+
+  initProfiles();
+});
+
+/*****************************/
+/***** INIT PAGE ON LOAD *****/
+/*****************************/
+(function() {
+  getPageInformation();
+  initProfiles();
+})();
